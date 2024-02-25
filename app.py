860cb590c74051595ba62d99c6fa05b8d51f4543
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-02-25 12:12:02 +0200

    initial functionality

--- app.py
+++ app.py
@@ -18,3 +18,19 @@ class Calculator:
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    a = int(request.form['first_number'])
+    b = int(request.form['second_number'])
+    operation = request.form['operation']
+    calculator = Calculator()
+    if operation == 'add':
+        result = calculator.add(a, b)
+    elif operation == 'subtract':
+        result = calculator.subtract(a, b)
+    elif operation == 'multiply':
+        result = calculator.multiply(a, b)
+    elif operation == 'divide':
+        result = calculator.divide(a, b)
+    return render_template('index.html', result=result, first_number=a, second_number=b, operation=operation)
